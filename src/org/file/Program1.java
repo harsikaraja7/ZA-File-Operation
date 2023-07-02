e16075b682e0a140ae5649e7b7e14c0088587022
harsikaraja7 <harsikaraja7@gmail.com>
2023-07-02 17:03:22 +0530

    File operation

--- src/org/file/Program1.java
+++ src/org/file/Program1.java
@@ -19,6 +19,7 @@ public static void main(String[] args) throws IOException {
     FileUtils.write(f1," QUESTIONS(Theory) \n 1. What is mean by File? In which package it is available? \n 2.What are the methods avaiable in File? \n 3. While creating a file if you not mention the format then under which format it will save the File? \n 4.What are the difference between append and updating the file? \n 5. How to compare paths of two files? \n 6. How to create a new file? \n 7. how to get last modified date of a file? \n 8. How to create a file in a specified directory? \n 9. How to check a file exist or not? \n 10. How to make a file read-only? ", false);
     List<String> readlines = FileUtils.readLines(f1);
     System.out.println(readlines);
+    System.out.println("done");
 }
 
 }
